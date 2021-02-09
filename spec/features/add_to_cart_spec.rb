de73e858f03d071484cf3ab0fc3bac17571996b4
Natasha Colusso <natashacolusso@gmail.com>
2021-02-09 22:11:29 +0000

    Feature: Capybara test for adding product to cart and nav cart count increase b 1

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can add item to cart", type: :feature, js:true do
+  
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They click on a specific product and see their details" do
+
+    visit root_path
+
+    expect(page).to have_css 'a', text: "My Cart (0)"
+    save_screenshot
+
+    find(:button, 'Add', match: :first).click
+
+    expect(page).to have_css 'a', text: "My Cart (1)"
+
+    save_screenshot
+  end
+end
