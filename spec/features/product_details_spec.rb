1b51a82d2adcf1d436d77bd6c36cf1f61f102817
Natasha Colusso <natashacolusso@gmail.com>
2021-02-09 21:57:23 +0000

    Feature: Capybara test added for navigation from home page to product detail page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can get product details", type: :feature, js:true do
+  
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They click on a specific product and see their details" do
+    #ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    
+    find(:link, 'Details', match: :first).click
+
+    expect(page).to have_css 'dt', text: "Quantity"
+
+    save_screenshot
+  end
+
+end
